59a9ecf93bdab1505fa0f8116576ccd0cdbc0058
anna.staniszewska <anna.staniszewska93@gmail.com>
2020-06-09 11:43:17 +0200

    Create Add an Event form.

---
+++ MyCalendar/MyCalendar/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using MyCalendar.Models;
+using MyCalendar.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MyCalendar.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel
+            {
+                Types = _context.Types.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MyCalendar/MyCalendar/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MyCalendar.Models;
+using System.Collections.Generic;
+
+namespace MyCalendar.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+
+        public int Type { get; set; }
+
+        public IEnumerable<Type> Types { get; set; }
+
+        public string Time { get; set; }
+    }
+}
