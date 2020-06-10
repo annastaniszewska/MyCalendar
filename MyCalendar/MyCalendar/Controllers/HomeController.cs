28f0949881d1480c0ecbbb2a0fb15ba1bca1aad5
anna.staniszewska <anna.staniszewska93@gmail.com>
2020-06-10 18:49:34 +0200

    Add name to user.

--- MyCalendar/MyCalendar/Controllers/HomeController.cs
+++ MyCalendar/MyCalendar/Controllers/HomeController.cs
@@ -1,16 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
+using MyCalendar.Models;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MyCalendar.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var userId = User.Identity.GetUserId();
+
+            var latestEvent = _context.Events
+                .Include(e => e.User)
+                .Where(e => e.UserId == userId)
+                .OrderByDescending(e => e.StartDate)
+                .FirstOrDefault();
+
+            return View(latestEvent);
         }
 
         public ActionResult About()
