bfd830de0a901490ad623c5b622e47de7007eb01
anna.staniszewska <anna.staniszewska93@gmail.com>
2020-07-29 14:25:09 +0200

    Adding domain model unit test,

---
+++ MyCalendar/MyCalendar.Tests/Domain/Models/EventTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalendar.Core.Models;
+
+namespace MyCalendar.Tests.Domain.Models
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestMethod]
+        public void Cancel_WhenCalled_ShouldSetIsCanceledToTrue()
+        {
+            var cycleEvent = new Event();
+
+            cycleEvent.Cancel();
+
+            cycleEvent.IsCanceled.Should().BeTrue();
+        }
+    }
+}
