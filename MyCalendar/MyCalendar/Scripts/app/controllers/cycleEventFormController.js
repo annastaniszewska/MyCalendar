06ff20cb135e8d05d670b8d09f3f2734eeae28fb
anna.staniszewska <anna.staniszewska93@gmail.com>
2020-08-05 11:54:17 +0200

    Changes applied to cycle event form to increase usability.

--- MyCalendar/MyCalendar/Scripts/app/controllers/cycleEventFormController.js
+++ MyCalendar/MyCalendar/Scripts/app/controllers/cycleEventFormController.js
@@ -1,36 +1,48 @@
 ﻿var CycleEventFormController = function () {
 
-    var init = function (dropdownId, eventTimeId, eventDateId) {
-        handleFormDisplay(dropdownId, eventTimeId, eventDateId);
+    var init = function (dropdownId, eventStartDate, eventEndDateId, eventTimeId) {
+        handleFormDisplay(dropdownId, eventStartDate, eventEndDateId, eventTimeId);
     };
 
-    var handleFormDisplay = function (dropdownId, eventTimeId, eventDateId) {
+    var handleFormDisplay = function (dropdownId, eventStartDate, eventEndDateId, eventTimeId) {
         $(function () {
 
-            if ($(dropdownId).find(":selected").text() !== "Ovulation") {
-                periodEventDisplay(eventTimeId, eventDateId);
+            if ($(dropdownId).find(":selected").text() !== "Ovulation" || $(dropdownId).find(":selected").text() !== "Period") {
+                noEventProvided(eventStartDate, eventEndDateId, eventTimeId);
             }
 
             $(dropdownId).change(function () {
                 var val = $(this).find(":selected").text();
 
-                if (val === "Ovulation") {
-                    ovulationEventDisplay(eventTimeId, eventDateId);
+                if (val == "Nothing Selected") {
+                    noEventProvided(eventStartDate, eventEndDateId, eventTimeId);
+                }
+                else if (val == "Ovulation") {
+                    ovulationEventDisplay(eventStartDate, eventTimeId, eventEndDateId);
                 } else {
-                    periodEventDisplay(eventTimeId, eventDateId);
+                    periodEventDisplay(eventStartDate, eventTimeId, eventEndDateId);
                 }
             });
         });
     };
 
-    var periodEventDisplay = function (eventTimeId, eventDateId) {
+    var noEventProvided = function (eventStartDate, eventEndDateId, eventTimeId) {
+        $(eventStartDate).hide()
+        $(eventEndDateId).hide();
         $(eventTimeId).hide();
-        $(eventDateId).show();
+
     };
 
-    var ovulationEventDisplay = function (eventTimeId, eventDateId) {
+    var periodEventDisplay = function (eventStartDate, eventTimeId, eventEndDateId) {
+        $(eventTimeId).hide();
+        $(eventStartDate).show();
+        $(eventEndDateId).show();
+    };
+
+    var ovulationEventDisplay = function (eventStartDate, eventTimeId, eventEndDateId) {
+        $(eventStartDate).show();
         $(eventTimeId).show();
-        $(eventDateId).hide();
+        $(eventEndDateId).hide();
     };
 
     return {
